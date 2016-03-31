871f73537c80e77056ed16def7247132144a432a
Karsten Schmidt <k@postspectacular.com>
2016-03-31 13:01:24 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject jogl232-foo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"jogamp" "http://jogamp.org/deployment/maven"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.jogamp.gluegen/gluegen-rt "2.3.2"]
                  [org.jogamp.jogl/jogl-all "2.3.2"]
                  [thi.ng/geom "0.0.1062"]]
-  :profiles {:osx {:dependencies [[org.jogamp.gluegen/gluegen-rt "2.3.2" :classifier "natives-macosx-universal"]
-                                  [org.jogamp.jogl/jogl-all "2.3.2" :classifier "natives-macosx-universal"]]}}
+  :profiles {:osx {:dependencies [[org.jogamp.gluegen/gluegen-rt "2.3.2"
+                                   :classifier "natives-macosx-universal"]
+                                  [org.jogamp.jogl/jogl-all "2.3.2"
+                                   :classifier "natives-macosx-universal"]]}}
   :main jogl232foo.core
   :aot :all)
